82be34e3e5ac0b9a5766dac2636ba19b9176d8ad
Mateusz Puczynski <mateusz.puczynski@sap.com>
2019-03-02 18:05:15 +0100

    add bundle analyzer and optimize code

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,44 +1,48 @@
 const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
 const path = require("path");
 const port = 8080;
 module.exports = merge.smart(common, {
-    devServer: {
-        overlay: true,
-        port: port,
-        quiet: true, //need this for FriendlyErrorsWebpackPlugin
-        watchContentBase: true,
-        contentBase: path.join(__dirname, "src"),
-        watchOptions: {
-            ignored: /node_modules/,
-        },
-    },
-    mode: "development",
-    devtool: "cheap-module-source-map",
+	devServer: {
+		overlay: true,
+		port: port,
+		quiet: true, //need this for FriendlyErrorsWebpackPlugin
+		watchContentBase: true,
+		contentBase: path.join(__dirname, "src"),
+		watchOptions: {
+			ignored: /node_modules/,
+		},
+	},
+	optimization: {
+		nodeEnv: "development",
+	},
+	mode: "development",
+	devtool: "cheap-module-source-map",
 
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                include: path.resolve("./src"),
-                loader: "babel-loader",
-                options: {
-                    cacheDirectory: true,
-                    cacheCompression: false,
-                    compact: false,
-                },
-            },
-        ],
-    },
-    plugins: [
-        new FriendlyErrorsWebpackPlugin({
-            compilationSuccessInfo: {
-                messages: [
-                    `You application is running here http://localhost:${port}`,
-                ],
-                notes: ["Good luck!"],
-            },
-        }),
-    ],
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				include: path.resolve("./src"),
+				loader: "babel-loader",
+				options: {
+					cacheDirectory: true,
+					cacheCompression: false,
+					compact: false,
+				},
+			},
+		],
+	},
+	plugins: [
+		new BundleAnalyzerPlugin(),
+		new FriendlyErrorsWebpackPlugin({
+			compilationSuccessInfo: {
+				messages: [`You application is running here http://localhost:${port}`],
+				notes: ["Good luck!"],
+			},
+		}),
+	],
 });
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,45 +1,55 @@
 const merge = require("webpack-merge");
 const path = require("path");
 const common = require("./webpack.common.js");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const ResourceHintWebpackPlugin = require("resource-hints-webpack-plugin");
 module.exports = merge.smart(common, {
-    mode: "production",
-    devtool: "source-map",
-    bail: true,
-    output: {
-        filename: "js/[name][hash:8].min.js",
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                include: path.resolve("./src"),
-                loader: "babel-loader",
-                options: {
-                    cacheDirectory: true,
-                    cacheCompression: true,
-                    compact: true,
-                },
-            },
-        ],
-    },
-    plugins: [
-        new CleanWebpackPlugin(["dist"]),
-        new ResourceHintWebpackPlugin(),
-        new HtmlWebPackPlugin({
-            template: "./public/index.html",
-            filename: "./index.html",
-            favicon: "./public/favicon.ico",
-            minify: {
-                collapseWhitespace: true,
-                removeComments: true,
-                removeRedundantAttributes: true,
-                removeScriptTypeAttributes: true,
-                removeStyleLinkTypeAttributes: true,
-                useShortDoctype: true,
-            },
-        }),
-    ],
+	mode: "production",
+	devtool: "source-map",
+	bail: true,
+	optimization: {
+		nodeEnv: "production",
+		concatenateModules: true,
+	},
+	output: {
+		filename: "js/[name][hash:8].min.js",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				include: path.resolve("./src"),
+				loader: "babel-loader",
+				options: {
+					cacheDirectory: true,
+					cacheCompression: true,
+					compact: true,
+				},
+			},
+		],
+	},
+	plugins: [
+		new BundleAnalyzerPlugin({
+			analyzerMode: "static",
+			openAnalyzer: false,
+			reportFilename: "../bundle_analyzer/report.html",
+		}),
+		new CleanWebpackPlugin(["dist", "bundle_analyzer"]),
+		new ResourceHintWebpackPlugin(),
+		new HtmlWebPackPlugin({
+			template: "./public/index.html",
+			filename: "./index.html",
+			favicon: "./public/favicon.ico",
+			minify: {
+				collapseWhitespace: true,
+				removeComments: true,
+				removeRedundantAttributes: true,
+				removeScriptTypeAttributes: true,
+				removeStyleLinkTypeAttributes: true,
+				useShortDoctype: true,
+			},
+		}),
+	],
 });
